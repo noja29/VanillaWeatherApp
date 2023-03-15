296b1b4cd3291453133877991f4c582ebbaeabf5
noja29 <joan409137185@gmail.com>
2023-03-15 20:26:21 +0100

    java script part 1

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = Math.round(response.data.main.temperature);
+  cityElement.innerHTML = response.data.name;
+}
+
+let apiKey = "b0b41c48cd830cf4e2d1ded6332ea8db";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
