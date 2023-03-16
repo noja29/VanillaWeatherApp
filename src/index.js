aee71bee54147e2b06234e4fa8b690ac1a98f00b
noja29 <joan409137185@gmail.com>
2023-03-16 19:30:14 +0100

    showing current weather

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temperature);
   cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.main.wind);
 }
 
 let apiKey = "73fo054d9ae81a22f93cdbtb4a8b9610";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
 axios.get(apiUrl).then(displayTemperature);
