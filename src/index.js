110c78d360a3294a112ea1f7405d4a4cb1333e4b
noja29 <joan409137185@gmail.com>
2023-03-21 20:34:49 +0100

    added search engine

--- src/index.js
+++ src/index.js
@@ -50,8 +50,18 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let city = "New York";
 
-let apiKey = "b0b41c48cd830cf4e2d1ded6332ea8db";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "b0b41c48cd830cf4e2d1ded6332ea8db";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("New York");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
